2e9cfdf1b1dcfdf0656d287e3b2226cffd618b45
Roxy <62872500+starblaster69@users.noreply.github.com>
2022-06-14 14:53:18 +0200

    prototype 2
    this one has a templated npc class, and functioning quest system. quest system has only 1 quest, functioning as template of sorts. it also has an interacteable npc's, and the beginnings of a dialogue display system. as ive primarily been working directly into the CLE project, ive removed all code written by my team partners in this version, because i dont want to claim their work as my own. you can verify this using the primary project's commit history.

---
+++ src/assets.ts
@@ -0,0 +1,90 @@
+import * as PIXI from 'pixi.js'
+import townImage from "./images/Map & Terrain/ZeldaWorld.png"
+import playerImage from "./images/PlayerCharacters/Player.png"
+import npcImageHolbewoner from "./images/NonPlayerCharacters/Holbewoner.png"
+import npcImageBunny from "./images/NonPlayerCharacters/Bunny.png"
+import knuppelImage from "./images/PlayerCharacters/playerWeapons/WoodenClub.png"
+import { Game } from './game'
+
+type AssetFile = { name: string, url: string }
+
+export class Assets extends PIXI.Loader {
+    public npcJson: any[] = []
+    private toLoad: AssetFile[] = []
+
+    public questsJson = [//TEMPORARY
+        { 
+            "questName" : "bunnyMurder",
+            "questStatus" : 0,
+            "questReward" : "magicSword",
+            "stages" : [
+                "not started",
+                "objective 1",
+                "objective 2",
+                "completed"
+            ]
+        }
+    ]
+
+    constructor(g: Game) {
+        super()
+
+        this.fetchRequest("npc")
+
+        //all the images and other files to be added to the loader, can be freely added to
+        this.toLoad = [
+            { name: 'townTexture', url: townImage },
+            { name: 'playerSprite', url: playerImage },
+            { name: 'Holbewoner', url: npcImageHolbewoner },
+            { name: 'Bunny', url: npcImageBunny },
+            { name: 'woodclubTexture', url: knuppelImage },
+            //{ name: 'attack', url: 'attack.json' } //attack.json does not exist. gives an error
+        ]
+
+        //adds all above assets
+        this.toLoad.forEach(asset => {
+            // Add to loader
+            this.add(asset.name, asset.url)
+        })
+
+        //shows loading progress and any errors in the console
+        this.onError.add((arg) => { console.error(arg) })
+        this.onProgress.add((loader) => this.showProgress(loader))
+        // call load and point to callback
+        this.load(() => g.loadCompleted())
+    }
+
+    //shows loading progress and any errors in the console
+    private showProgress(loader: PIXI.Loader) {
+        console.log(`Loading ${loader.progress}%`)
+    }
+
+    private fetchRequest(type: string) {
+        fetch(`../npcs.json`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then((data: any) => this.npcFetchHeader(data))
+            .catch((err: string) => this.AJAXErrorHandler(err));
+    }
+
+    /** fetch request error handler */
+    private AJAXErrorHandler(data: string) {
+        console.error("AJAX load error: " + data)
+    }
+
+    // NPC Json fetch success handler, loads everything into a global array.
+    private npcFetchHeader(data: any) {
+        //console.log(data.length)
+        //console.log(data)
+        for (let i = 0; i < data.length; i++) {
+            // console.log(data)
+            this.npcJson.push(data[i])
+            //console.log(this.npcJson[i])
+        }
+        // console.log(this.npcJson)
+    }
+}
